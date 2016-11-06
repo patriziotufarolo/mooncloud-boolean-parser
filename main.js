0e2b51ace3c98ea4ed5343cbeb38026d5cf4767b
Patrizio Tufarolo <patrizio@tufarolo.eu>
2016-11-06 20:44:44 +0100

    fourth commit

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ parser.yy.parseError = (message, hash) => {
     throw new SyntaxError(message, hash);
 }
 (() => {
-    let module = angular.module('mooncloud.booleanParser')
+    let module = angular.module('mooncloudBooleanParser')
     module.factory('mooncloudBooleanParserService', function() {
         return {
             parse: (formula) => parser.parse(formula)
